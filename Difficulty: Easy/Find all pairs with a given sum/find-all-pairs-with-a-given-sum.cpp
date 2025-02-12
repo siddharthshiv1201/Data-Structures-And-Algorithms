91c99f1167db607a074067eecfc25c9a1ad49fed
Siddharth Shiv <118655632+siddharthshiv1201@users.noreply.github.com>
2025-02-12 22:01:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find all pairs with a given sum/find-all-pairs-with-a-given-sum.cpp
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to find all pairs with given sum.
+    vector<pair<int, int>> allPairs(int target, vector<int> &arr1, vector<int> &arr2) {
+       vector<pair<int, int>> result; 
+
+    // Sort arr1 to maintain output order
+    sort(arr1.begin(), arr1.end());
+
+    // Use unordered_map to store arr2 elements with their counts
+    unordered_map<int, int> freq;
+    for (int num : arr2) {
+        freq[num]++;
+    }
+
+    // Find pairs
+    for (int num : arr1) {
+        int complement = target - num;
+        if (freq.find(complement) != freq.end()) {
+            for (int i = 0; i < freq[complement]; i++) {
+                result.push_back({num, complement});
+            }
+        }
+    }
+
+    return result; 
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int x;
+        cin >> x;
+        cin.ignore();
+
+        vector<int> arr1;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr1.push_back(number);
+        }
+        vector<int> arr2;
+        string input2;
+        getline(cin, input2); // Read the entire line for the array elements
+        stringstream ss2(input2);
+        int number2;
+        while (ss2 >> number2) {
+            arr2.push_back(number2);
+        }
+
+        Solution ob;
+        vector<pair<int, int>> vp = ob.allPairs(x, arr1, arr2);
+        int sz = vp.size();
+        if (sz == 0)
+            cout << -1 << endl;
+        else {
+            for (int i = 0; i < sz; i++) {
+                if (i == 0)
+                    cout << vp[i].first << " " << vp[i].second;
+                else
+                    cout << ", " << vp[i].first << " " << vp[i].second;
+            }
+            cout << endl;
+        }
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
