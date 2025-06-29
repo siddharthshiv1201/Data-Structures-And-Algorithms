122013aace5d080dccacc579910ef2ac0cc0e95d
Siddharth Shiv <118655632+siddharthshiv1201@users.noreply.github.com>
2025-06-29 21:28:17 +0530

    Time: 11 ms (26.84%), Space: 14.1 MB (94.8%) - LeetHub

---
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        vector<int> v;
+        int n=nums.size();
+
+        for(int i=0; i<n; i++){
+            int count=0;
+            for(int j=0; j<n; j++){
+                if(nums[i]>nums[j]){
+                    count++;
+                }
+            }
+            v.push_back(count);
+        }
+        return v;
+
+    }
+};
