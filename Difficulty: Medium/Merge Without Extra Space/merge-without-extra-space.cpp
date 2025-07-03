90f76c48a37cd8b2163f84b54dec06d6bef315f0
Siddharth Shiv <118655632+siddharthshiv1201@users.noreply.github.com>
2025-07-03 21:31:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge Without Extra Space/merge-without-extra-space.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    void mergeArrays(vector<int>& a, vector<int>& b) {
+        // code here
+        int n=a.size();
+        int m=b.size();
+        int i=n-1;
+        int j=0;
+        while(i>=0 && j<m){
+            if(a[i]>b[j]){
+                swap(a[i],b[j]);
+            }
+            i--;
+            j++;
+        }
+        sort(a.begin(),a.end());
+        sort(b.begin(),b.end());
+    }
+};
